556f5fc655ecbd58830be7fd6db392fb79b27009
matt0 <matt@planet-labs.com>
2014-02-27 22:44:31 -0800

    taaaaaabs

--- server.go
+++ server.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-  "github.com/codegangsta/martini"
-  "github.com/martini-contrib/render"
+    "github.com/codegangsta/martini"
+    "github.com/martini-contrib/render"
 )
 
 func main() {
-  m := martini.Classic()
-  m.Use(render.Renderer())
+    m := martini.Classic()
+    m.Use(render.Renderer())
 
-  m.Get("/", func(r render.Render) {
-    r.HTML(200, "base", "yolo")
-  })
+    m.Get("/", func(r render.Render) {
+        r.HTML(200, "base", "yolo")
+    })
 
-  m.Run()
+    m.Run()
 }
